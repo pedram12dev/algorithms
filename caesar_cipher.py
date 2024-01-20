26b7599c4ff1f3e426c60eaafa09556b96b7e4be
pedram-macbook <pedram@email.com>
2024-01-20 11:56:50 +0330

    write caesar cipher algorithm

---
+++ caesar_cipher.py
@@ -0,0 +1,25 @@
+from string import ascii_letters
+
+
+def encrypt(string, key):
+    alpha = ascii_letters
+    result = ''
+
+    for ch in string:
+        if ch not in alpha:
+            result += ch
+        else:
+            new_key = (alpha.index(ch) + key) % len(alpha)
+            result += alpha[new_key]
+    return result
+
+
+print(encrypt('pedram', 4))
+
+
+def decrypt(string, key):
+    key *= -1
+    return encrypt(string, key)
+
+
+print(decrypt('tihveq', 4))
